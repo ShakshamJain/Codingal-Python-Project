43dd2604a86e685c92a7d55ad4fe1b43c54a92d4
ShakshamJain <shakshamj0511@gmail.com>
2026-07-05 19:21:32 +0800

    Birhtdaysorter

---
+++ storing_brithdays/main.py
@@ -0,0 +1,11 @@
+Aakruti = "24 March 2011"
+Shaurya = "24 April 2011"
+Aadik = "01 September 2012"
+Sidarth = "10 October 2010"
+Jainam = "08 December 2010"
+
+print("Aakruti's birthday is on the",Aakruti)
+print("Shaurya's birthday is on the",Shaurya)
+print("Aadik's birthday is on the",Aadik)
+print("Sidarth's birthday in on the",Sidarth)
+print("Jainam's birthday in on the",Jainam)
