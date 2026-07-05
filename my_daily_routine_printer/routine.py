5c86bbd254f587425ee68726d9a8b9dd9a56e77a
ShakshamJain <shakshamj0511@gmail.com>
2026-07-05 17:38:21 +0800

    DailyRoutineProject

---
+++ my_daily_routine_printer/routine.py
@@ -0,0 +1,16 @@
+print("My Daily Routine")
+
+# Step 1: Waking Up
+print("I wake up at 5:30 AM.")
+
+# Step 2: Going to School
+print("I go to school at 7:15 AM.")
+
+# Step 3: Relaxing After School
+print("After school, I relax and spend time with my family.")
+
+# Step 4: Studying
+print("I study for 1.5 hours at night.")
+
+# Calculating Total Study Time in a Week
+print("Total Study Hours in a Week:", 1.5*7)
